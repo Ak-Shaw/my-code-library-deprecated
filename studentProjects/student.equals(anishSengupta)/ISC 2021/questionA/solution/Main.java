85daf717d1cdbe2b040a4c96ff2fdd027d26af74
Ak-Shaw <ayushshawz@gmail.com>
2020-09-28 19:43:16 +0530

    update

---
+++ studentProjects/student.equals(anishSengupta)/ISC 2021/questionA/solution/Main.java
@@ -0,0 +1,16 @@
+package questionA.solution;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        double a = 5.0;
+        double b = 6.0;
+        double h = 7.0;
+
+        Area area = new Area(a, b, h);
+
+        area.show();
+    }
+    
+}
---
+++ studentProjects/student.equals(anishSengupta)/ISC 2021/questionA/solution/Perimeter.java
@@ -0,0 +1,23 @@
+package questionA.solution;
+
+public class Perimeter {
+    
+    double a;
+    double b;
+
+    Perimeter(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public double calculate() {
+        return 2 * (a + b);
+    }
+
+    public void show() {
+
+        System.out.println("Length: " + this.a);
+        System.out.println("Breadth: " + this.b);
+        System.out.println("Perimeter: " + calculate());
+    }
+}
